f2e65936f44c309ff616f1044f2c7444d0b2a168
Dom Dwyer <dom@itsallbroken.com>
2022-08-24 08:10:02 +0000

    version 0.1.3

---
+++ R/query_range.R
@@ -0,0 +1,118 @@
+#' Evaluate an expression query over a range of time.
+#'
+#' @param query A PromQL query.
+#' @param start A RFC3339 timestamp string, numerical unix timestamp, or POSIXct
+#'     object.
+#' @param end A RFC3339 timestamp string, numerical unix timestamp, or POSIXct
+#'     object.
+#' @param host An optional host - defaulting to `http://127.0.0.1:9090`
+#' @param step An optional query resolution step width, defaulting to `10s`
+#' @param timeout An optional query timeout value, defaulting to server-side
+#'     limit. Note this timeout is capped to the server-side value.
+#' @return A tibble of all series returned by the server, with nested
+#'     measurements.
+#' @examples
+#' \dontrun{
+#' # Run a simple range query against the specified host.
+#' query_range(
+#'   "up",
+#'   "2022-08-20T00:00:00Z",
+#'   "2022-08-21T00:00:00Z",
+#'   host = "http://127.0.0.1:9090"
+#' )
+#'
+#' # Run a server-side aggregation query, using the default local host.
+#' query_range(
+#'   "rate(http_requests_total[5m])",
+#'   "2022-08-20T00:00:00Z",
+#'   "2022-08-21T00:00:00Z"
+#' )
+#'
+#' # Specify the time range using POSIXct objects, and set the optional "step"
+#' query_range(
+#'   "rate(http_requests_total[5m])",
+#'   strptime(
+#'     "2022-08-20T20:10:30",
+#'     format = "%Y-%m-%dT%H:%M:%S"
+#'   ),
+#'   strptime(
+#'     "2022-08-21T20:10:30",
+#'     format = "%Y-%m-%dT%H:%M:%S"
+#'   ),
+#'   step = "30s"
+#' )
+#'
+#' # Specify the time range using unix timestamps, and set an optional "timeout"
+#' query_range(
+#'   "rate(http_requests_total[5m])",
+#'   1660989814,
+#'   1661076214,
+#'   timeout = "60s"
+#' )
+#' }
+#' @export
+query_range <- function(query,
+                        start,
+                        end,
+                        host = "http://127.0.0.1:9090",
+                        step = "10s",
+                        timeout = NA) {
+  # Construct the v1 API query URL
+  url <- build_url(host, query, start, end, step, timeout) # nolint.
+
+  # Send the request and validate it succeeded
+  r <- httr::GET(url)
+  d <- httr::content(r, "parsed")
+  if (r$status_code != 200 || d$status == "error") {
+    stop("query failure (HTTP ", r$status_code, "): ", d$error)
+  }
+  if (d$data$resultType != "matrix") {
+    stop("unexpected result type ", d$data$resultType)
+  }
+
+  # Populate the output tibble
+  df <- tibble::tibble()
+  for (r in d$data$result) {
+    # Capture the name + labels
+    row <- tibble::as_tibble(r$metric)
+
+    # If an aggregate produces a single series, the "metric" field is
+    # unpopulated, but a row is necessary below
+    if (nrow(row) == 0) {
+      row <- tibble::tibble(
+        `__name__` = query,
+      )
+    }
+
+    # Compute the number of measurements in the tuple list
+    values <- unlist(r$value)
+    n_rows <- length(values) / 2
+
+    # Construct a tibble of values by first unrolling the list, and then turning
+    # it into a matrix, populated row wise.
+    values <- tibble::as_tibble(
+      matrix(
+        values,
+        ncol = 2,
+        nrow = n_rows,
+        byrow = TRUE
+      ),
+      .name_repair = ~ c("timestamp", "value")
+    )
+
+    # Convert the string values into useful types
+    values$timestamp <- as.POSIXct(
+      as.numeric(values$timestamp),
+      origin = "1970-01-01"
+    )
+    values$value <- as.numeric(values$value)
+
+    # Nest the values tibble in the output row
+    row$values <- list(values)
+
+    # Add this metric to the output
+    df <- rbind(df, row)
+  }
+
+  return(df)
+}
---
+++ tests/testthat/test-query_range.R
@@ -0,0 +1,124 @@
+with_mock_api({
+  test_that("query_range returns data", {
+    #
+    # This test query, and the response, was sourced from the prometheus docs:
+    #
+    #   https://prometheus.io/docs/prometheus/latest/querying/api/#range-queries
+    #
+
+    got <- expect_silent(query_range(
+      "up",
+      "2015-07-01T20:10:30.781Z",
+      "2015-07-01T20:11:00.781Z",
+      host = "http://127.0.0.1:9090/", # Explicit host
+      step = "15s"
+    ))
+
+    # Expected output tibble:
+    #
+    #    # A tibble: 2 x 4
+    #      `__name__` job        instance       values
+    #      <chr>      <chr>      <chr>          <list>
+    #    1 up         prometheus localhost:9090 <tibble [3 x 2]>
+    #    2 up         node       localhost:9091 <tibble [3 x 2]>
+    #
+    # Unnested:
+    #
+    #   # A tibble: 6 x 5
+    #     `__name__` job        instance       time       value
+    #     <chr>      <chr>      <chr>          <chr>      <chr>
+    #   1 up         prometheus localhost:9090 1435781430 1
+    #   2 up         prometheus localhost:9090 1435781445 1
+    #   3 up         prometheus localhost:9090 1435781460 1
+    #   4 up         node       localhost:9091 1435781430 0
+    #   5 up         node       localhost:9091 1435781445 0
+    #   6 up         node       localhost:9091 1435781460 1
+    #
+
+    want <- tibble::tibble(
+      `__name__` = c("up", "up"),
+      job = c("prometheus", "node"),
+      instance = c("localhost:9090", "localhost:9091"),
+      values = list(
+        tibble::tibble(
+          timestamp = as.POSIXct(
+            c(
+              1435781430,
+              1435781445,
+              1435781460
+            ),
+            origin = "1970-01-01"
+          ),
+          value = c(1, 1, 1)
+        ),
+        tibble::tibble(
+          timestamp = as.POSIXct(
+            c(
+              1435781430,
+              1435781445,
+              1435781460
+            ),
+            origin = "1970-01-01"
+          ),
+          value = c(0, 0, 1)
+        )
+      ),
+    )
+
+    expect_equal(got, want)
+  })
+
+  test_that("server side aggregate", {
+    # nolint start: line_length_linter
+    q <- "sum by (handler, result) (rate(dml_handler_write_duration_seconds_count{}[1m]))"
+    # nolint end
+
+    got <- query_range(
+      q,
+      "2022-08-19T00:00:00Z",
+      "2022-08-20T00:00:00Z",
+    ) # NOTE: uses default host
+
+    expect_equal(nrow(got), 10)
+    expect_equal(unique(got$handler), c(
+      "parallel_write",
+      "partitioner",
+      "request",
+      "schema_validator",
+      "sharded_write_buffer"
+    ))
+    expect_equal(unique(got$result), c("error", "success"))
+    expect_equal(nrow(got$values[[1]]), 8641)
+    expect_equal(colnames(got$values[[1]]), c("timestamp", "value"))
+  })
+
+  test_that("single series aggregate", {
+    # nolint start: line_length_linter
+    q <- "sum(rate(dml_handler_write_duration_seconds_bucket{handler=\"sharded_write_buffer\"}[1m]))"
+    # nolint end
+
+    got <- query_range(
+      q,
+      "2022-08-21T00:00:00Z",
+      "2022-08-22T00:00:00Z"
+    )
+
+    expect_equal(got$`__name__`[[1]], q)
+    expect_equal(nrow(got$values[[1]]), 8641)
+    expect_equal(colnames(got$values[[1]]), c("timestamp", "value"))
+  })
+
+  test_that("error messages are returned to the user", {
+    # nolint start: line_length_linter
+    q <- "sum(rate(dml_handler_write_duration_seconds_bucket{handler=\"bananas\"}[1m]))"
+
+    expect_snapshot_error(
+      query_range(
+        q,
+        "2022-08-21T00:00:00Z",
+        "2022-08-22T00:00:00Z"
+      )
+    )
+    # nolint end
+  })
+})
